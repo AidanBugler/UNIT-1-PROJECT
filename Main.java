90dce9e522d95b41e6e06cc27c3deac4da548223
AidanBugler <buglaida982@gotvdsb.ca>
2020-11-25 15:13:38 +0000

    ADDED MENU

--- Main.java
+++ Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+ //Scanner to find inputs from user   
+Scanner input = new Scanner(System.in);
+//Variables 
+int menuChoice;
+
+System.out.println("What would you like to know?");
+    System.out.println("1. Multiplication table 2. Addition table");
+    System.out.println("3. AND logic gate table 4. OR logic gate table");
+    menuChoice = Integer.parseInt(input.nextLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
 }
