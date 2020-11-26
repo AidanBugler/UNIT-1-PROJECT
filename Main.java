33011988b18dae47ba68d4340b89e4f1d292bc30
AidanBugler <buglaida982@gotvdsb.ca>
2020-11-26 17:05:17 +0000

    ADDED OR, AND TABLES

--- Main.java
+++ Main.java
@@ -6,36 +6,37 @@ class Main {
     Scanner input = new Scanner(System.in);
     // Variables
     int menuChoice;
-    int [] multColl = {1,2,3,4,5,6,7,8,9,10,11,12};
-    int [] multRow = {1,2,3,4,5,6,7,8,9,10,11,12};
-    int [] addColl = {10,20,30,40,50,60,70,80,90,100};
-    int [] addRow = {1,2,3,4,5,6,7,8,9,10};
+    int [] multiplicationColl = {1,2,3,4,5,6,7,8,9,10,11,12};
+    int [] multiplicationRow = {1,2,3,4,5,6,7,8,9,10,11,12};
+    int [] addingColl = {10,20,30,40,50,60,70,80,90,100};
+    int [] addingRow = {1,2,3,4,5,6,7,8,9,10};
     
-    
+    //This is the menun system that lets the user pick what they want to see
     do {
       System.out.println("What would you like to know?");
       System.out.println("1. Multiplication table 2. Addition table");
       System.out.println("3. AND logic gate table 4. OR logic gate table");
       System.out.println("5. Exit system");
       menuChoice = Integer.parseInt(input.nextLine());
+     //This checks if the user wants to see something or if they want to exit the program, if they do want to see something then it checks again what option they want to see
       if (menuChoice == 1 || menuChoice == 2 || menuChoice == 3 || menuChoice == 4)
        {
           if (menuChoice == 1) 
          {
-          System.out.println("this is the Multiplication table");
+          System.out.println("This is the Multiplication table");
           for(int i=0;i<12;i+=1)
           { 
           
             for (int j=0;j<12;j+=1)
             {
-              System.out.print(multRow[j]*multColl[i]+" ");
+              System.out.print(multiplicationRow[j]*multiplicationColl[i]+"  ");
             }
             System.out.println();
           }
          }
           if (menuChoice == 2) 
          {
-          System.out.println("this is the Addition table");
+          System.out.println("This is the Addition table");
           for(int j=0;j<10;j+=1)
           { 
             for (int i=0;i<=100;i+=10)
@@ -47,11 +48,21 @@ class Main {
          }
           if (menuChoice == 3) 
          {
-          System.out.println("this is the AND logic gate table");
+          System.out.println("This is the AND logic gate table");
+          System.out.println("Condition 1 | Condition 2 | AND ");
+          System.out.println("   True     |    True     | True ");
+          System.out.println("   True     |    False    | False ");
+          System.out.println("   False    |    True     | False ");
+          System.out.println("   False    |    False    | False ");
          }
           if (menuChoice == 4) 
          {
-          System.out.println("this is OR logic gate table");
+          System.out.println("This is OR logic gate table");
+          System.out.println("Condition 1 | Condition 2 | OR ");
+          System.out.println("   True     |    True     | True ");
+          System.out.println("   True     |    False    | True ");
+          System.out.println("   False    |    True     | True ");
+          System.out.println("   False    |    False    | False ");
          }
        }
       if (menuChoice == 5) 
